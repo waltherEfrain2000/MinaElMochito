716fd64baa2f68212972a40ef1660e9c5220c643
walther efrain <waltherulloa95@gmail.com>
2021-03-25 16:45:39 -0600

    creacion de constraints para las tablas y comienzo de las restricciones

--- ProyectoMinaELMochito/ProyectoMinaELMochito/BdMinaMochito.sql
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/BdMinaMochito.sql
@@ -1,3 +1,8 @@
+/*Base de datos Mina el MOchito*/
+
+use tempdb
+go
+
 create database MinaElMochito
 go
 
@@ -7,124 +12,216 @@ go
 create schema Minas
 go
 
+create schema Usuarios
+go
 
 create table Minas.Genero
 (
-	idGenero int identity primary key not null,
-	descripcion varchar(1)
+	idGenero int identity not null,
+	descripcion varchar(1),
+	Constraint PK_Genero_id
+	       primary key clustered(idGenero)
 )
 go
 
 create table Minas.EstadoVehiculo
 (
-	idEstado int identity primary key not null,
-	descripcion varchar(15)
+	idEstado int identity not null,
+	descripcion varchar(15),
+	Constraint PK_EstadoVehiculo_id
+	       primary key clustered(idEstado)
 )
 go
 
 create table Minas.cargo
 (
-	idCargo int identity primary key not null,
-	descripcion varchar(15)
+	idCargo int identity not null,
+	descripcion varchar(15),
+	Constraint PK_Cargo_id
+	       primary key clustered(idCargo)
 )
 go
 
 create table Minas.Mineral
 (
-	idMineral int identity primary key not null,
+	idMineral int identity  not null,
 	descripcion varchar(15),
-	precio numeric(18,2)	
+	precio numeric(18,2),
+	Constraint PK_Mineral_id
+	       primary key clustered(idMineral)
 )
 go
 
 create table Minas.Vehiculo
 (
-	idVehiculo int identity primary key not null,
+	idVehiculo int identity  not null,
 	marca varchar(15),
 	modelo varchar(15),
 	placa varchar(15) not null,
 	color varchar(15),
-	estado int Foreign key references Minas.EstadoVehiculo(idEstado)
-)
+	estado int ,
+      Constraint PK_Vehiculo_id
+	       primary key clustered(idVehiculo),    
+   
+   constraint fk_vehiculo_estado
+	foreign key (estado)
+	references Minas.EstadoVehiculo(idEstado),
+) 
 go
 
 create table Minas.Empleado
 (
-	IdEmpleado int identity primary key not null,
+	IdEmpleado int identity not null,
 	identidad varchar(13) not null,
 	primerNombre varchar(20) not null,
 	segundoNombre varchar(20),
 	primerApellido varchar(20) not null,
 	segundoApellido varchar(20),
 	edad int,
-	idGenero int Foreign key references Minas.Genero(idGenero),
+	idGenero int ,
 	direccion  varchar(50),
-	idCargo int Foreign key references Minas.cargo(idCargo),
+	idCargo int,
 	estado  varchar(15)
+
+	 Constraint PK_Empleado_id
+	       primary key clustered(IdEmpleado),
+	  
+	    constraint fk_Empleado_idGenero
+	foreign key (idGenero)
+	references Minas.Genero(idGenero),
+
+      constraint fk_vehiculo_idcargo
+	foreign key (idCargo)
+	references Minas.Cargo(idCargo)
 )
 go
 
 create table Minas.viajeInterno
 (
-	idViaje int identity primary key not null,
-	idVehiculo int Foreign key references Minas.Vehiculo (idVehiculo),
+	idViaje int identity not null,
+	idVehiculo int not null,
 	precio numeric(18,2),
-	idEmpleado int Foreign key references Minas.Empleado(IdEmpleado)
+	idEmpleado int not null,
+
+	
+	 Constraint PK_viaje_idViaje
+	       primary key clustered(idViaje),
+
+  constraint fk_ViajeInterno_idVehiculo
+	foreign key (idVehiculo)
+	references Minas.Vehiculo(idVehiculo),
+
+	  constraint fk_ViajeInterno_idEmpleado
+	foreign key (idEmpleado)
+	references Minas.Empleado(idEmpleado)
+	  
 )
 go
 
 create table Minas.Produccion
 (
-	idProduccion int identity primary key not null,
-	idViaje int Foreign key references Minas.viajeInterno(idViaje),
-	idMineral int Foreign key references Minas.Mineral(idMineral),
-	precio numeric(18,2)	
+	idProduccion int identity not null,
+	idViaje int not null,
+	idMineral int not null,
+	precio numeric(18,2),
+	
+		
+	 Constraint PK_Produccion_idProduccion
+	       primary key clustered(idProduccion),
+
+  constraint fk_Produccion_idViaje
+	foreign key (idViaje)
+	references Minas.viajeInterno(idViaje),
+
+	  constraint fk_Produccion_idMineral
+	foreign key (idMineral)
+	references Minas.Mineral(idMineral)
 )
 go
 
 create table Minas.Invetario
 (
-	idInventario int identity primary key not null,
+	idInventario int identity not null,
 	descripcion varchar(15),
-	cantidad int
+	cantidad int,
+
+	 Constraint PK_Inventario_idInventario
+	       primary key clustered(idInventario),
+
 )
 go
 
 create table Minas.InventarioMineral
 (
-	idMineral int Foreign key references Minas.Mineral(idMineral)not null,
+	idMineral int not null,
 	peso decimal(18,2),
 	fechaActualizacion datetime,
 	Total decimal(18,2)
+
+	  constraint fk_InventarioMineral_idMineral
+	foreign key (idMineral)
+	references Minas.Mineral(idMineral)
 )
 go
 
 create table Minas.Salida
 (
-	idSalida int identity primary key not null,
-	idProducto int Foreign key references Minas.Mineral(idMineral),
+	idSalida int identity  not null,
+	idmineral int ,
 	cantidad decimal(18,2),
 	Total decimal(18,2),
 	detalleVenta varchar(50),
-	fechaSalida datetime
+	fechaSalida datetime,
+
+	Constraint PK_Salida_idSalida
+	       primary key clustered(idSalida),
+
+  constraint fk_Salidas_idProducto
+	foreign key (idmineral)
+	references Minas.Mineral(idMineral),
 )
 go
 
 create table Minas.Entrada
 (
-	idEntrada int identity primary key not null,
-	idProducto int Foreign key references Minas.Mineral(idMineral),
+	idEntrada int identity  not null,
+	idProducto int ,
 	cantidad decimal(18,2),
 	Total decimal(18,2),
 	detalleEntrada varchar(50),
-	fechaEntrada datetime
+	fechaEntrada datetime,
+
+	Constraint PK_Entrada_idEntrada
+	       primary key clustered(idEntrada),
+
+  constraint fk_Entrada_Producto
+	foreign key (idProducto)
+	references Minas.Mineral(idMineral),
 )
 go
 
+Create table Usuarios.Usuario
+(
+  id INT NOT NULL IDENTITY (200, 1),
+	nombreCompleto VARCHAR(255) NOT NULL,
+	username VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	rol char (25),
+	estado BIT NOT NULL,
+
+	CONSTRAINT PK_Usuario_id
+		PRIMARY KEY CLUSTERED (id)
 
 
+)
 
 
+------------------------------restricciones
+
+ALTER TABLE Usuarios.Usuario WITH CHECK
+	ADD CONSTRAINT CHK_Usuarios_USuarios$RolUsuario
+	CHECK (rol IN('ADMINISTRADOR', 'EMPLEADODETURNO'))
+GO
 
 
 ----------------------------------------------------------------Inserciones------------------------------------------------------
