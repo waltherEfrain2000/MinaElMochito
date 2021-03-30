e889fdac967e9bae012ccda681a5b936d699839b
walther efrain <waltherulloa95@gmail.com>
2021-03-29 23:25:37 -0600

    creacion del login interfaz(material design) y funcionalidad con una clase llamada "User"

---
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/Login.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoMinaELMochito
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private User usuario = new User();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+                //System.Media.SystemSounds.Beep.Play();
+                //System.Media.SystemSounds.Hand.Play();
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            System.Media.SystemSounds.Question.Play();
+            try
+            {
+                // Implementar la búsqueda del usuario desde la clase Usuario
+                User elUsuario = usuario.BuscarUsuario(txtUsername.Text);
+
+                // Verificar si el usuario existe y si el rol es diferente al rol empleado de turno de esta manera se identifica quye es un usuario administrador
+                if (elUsuario.Username == null)
+                    MessageBox.Show("El usuario o la contraseña no es correcta. Favor verificar.", "Usuario o contraseña incorrecta", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                else
+                {
+                    // Verificar que la contraseña ingresada es igual a la contraseña
+                    // almacenada en la base de datos
+                    if (elUsuario.Password == pwbPassword.Password && elUsuario.Estado && elUsuario.Rol != "EmpleadoDeTurno")
+                    {
+                        // Mostrar el formulario de menú principal
+                        //MenuPrincipal menu = new MenuPrincipal(elUsuario.NombreCompleto);
+                        //menu.Show();
+                        MessageBox.Show($"Bienvenido {elUsuario.NombreCompleto} eres un Usuario Administrador", "Usuario ADMINISTRADOR", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        Close();
+                    }
+                    else if (elUsuario.Password == pwbPassword.Password && elUsuario.Estado)
+                    {
+                        MessageBox.Show($"Bienvenido {elUsuario.NombreCompleto} eres un Empleado de turno", "Usuario Emplado de Turno", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        Close();
+                    }
+
+
+                    else if (!elUsuario.Estado)
+                        MessageBox.Show("Tu usuario se encuentra innactivo. Favor comunicarte con el personal de IT");
+                    else
+                        MessageBox.Show("El usuario o la contraseña no es correcta. Favor verificar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Ha ocurrido un error al momento de realizar la consulta...");
+                MessageBox.Show(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnLogin_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+
+            //System.Media.SystemSounds.Hand.Play();
+
+
+        }
+
+
+        private void txtUsername_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            txtUsername.Text = String.Empty;
+        }
+
+        private void pwbPassword_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            pwbPassword.Password = String.Empty;
+        }
+
+        private void PackIconMaterial_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            pwbPassword.Visibility = Visibility.Hidden;
+            icon.Visibility = Visibility.Visible;
+            txtpassword.Visibility = Visibility.Visible;
+            txtpassword.Text = pwbPassword.Password;
+            iconeye.Visibility = Visibility.Hidden;
+
+        }
+
+        private void PackIconMaterial_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            pwbPassword.Password = txtpassword.Text;
+            pwbPassword.Visibility = Visibility.Visible;
+            icon.Visibility = Visibility.Hidden;
+            txtpassword.Visibility = Visibility.Hidden;
+
+            iconeye.Visibility = Visibility.Visible;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/User.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProyectoMinaELMochito
+{
+    class User
+    {
+        // Variables miembro
+        private static string connectionString = ConfigurationManager.ConnectionStrings["ProyectoMinaELMochito.Properties.Settings.MinaMochitoConnection"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        // Propiedades
+        public int Id { get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Rol { get; set; }
+        public bool Estado { get; set; }
+
+        // Constructores
+        public User() { }
+
+        public User(string nombreCompleto, string username, string password, string rol, bool estado)
+        {
+            NombreCompleto = nombreCompleto;
+            Username = username;
+            Password = password;
+            Rol = rol;
+            Estado = estado;
+        }
+
+        // Métodos
+
+        /// <summary>
+        /// Verifica si las credenciales de inicio de sesión son correctas.
+        /// </summary>
+        /// <param name="username">El nombre del usuario</param>
+        /// <returns>Los datos del usuario</returns>
+        public User BuscarUsuario(string username)
+        {
+            // Crear el objeto que almacena la información de los resultados
+            User usuario = new User();
+
+            try
+            {
+                // Query de selección
+                string query = @"SELECT * FROM Usuarios.Usuario
+                                 WHERE username = @username";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("@username", username);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del usuario si la consulta retorna valores
+                        usuario.Id = Convert.ToInt32(rdr["id"]);
+                        usuario.NombreCompleto = rdr["nombreCompleto"].ToString();
+                        usuario.Username = rdr["username"].ToString();
+                        usuario.Password = rdr["password"].ToString();
+                        usuario.Rol = rdr["rol"].ToString();
+                        usuario.Estado = Convert.ToBoolean(rdr["estado"]);
+                    }
+                }
+
+                // Retornar el usuario con los valores
+                return usuario;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+    }
+}
