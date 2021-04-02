7d69ac65e5c43dad1dce7cd1030596cbf6889bbb
abdiel Giron <abdielgiron32@gmail.com>
2021-04-02 17:34:55 -0600

    Creacion del Formulario y Clase para los Vehiculos

---
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/Vehiculos.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoMinaELMochito
+{
+    /// <summary>
+    /// Lógica de interacción para Vehiculos.xaml
+    /// </summary>
+    public partial class Vehiculos : Window
+    {
+        public Vehiculos()
+        {
+            InitializeComponent();
+        }
+
+        private void DgvVehiculos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
