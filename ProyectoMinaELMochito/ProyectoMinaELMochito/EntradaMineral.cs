c666687753d13c7d4d3c253f57f0ab43a5300a8a
walther efrain <waltherulloa95@gmail.com>
2021-04-15 17:11:17 -0600

    Creacion del historial entradas con grafico de dona

---
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/EntradaMineral.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProyectoMinaELMochito
+{
+    class EntradaMineral
+    {
+
+
+        private static string connectionString = ConfigurationManager.ConnectionStrings["ProyectoMinaELMochito.Properties.Settings.MinaConnectionString"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        // Propiedades
+        public int IdEntrada { get; set; }
+        public int IdMineral { get; set; }
+
+        public double Cantidad { get; set; }
+
+        public Decimal Total { get; set; }
+
+        public string DetalleEntrada { get; set; }
+        public DateTime FechaDeEntrada { get; set; }
+
+
+
+
+        // Constructores
+        public EntradaMineral() { }
+
+        public EntradaMineral(int idEntrada, int idMineral, double peso, decimal total, string detalleEntrada, DateTime fechaActualizacion)
+        {
+            IdEntrada = idEntrada;
+            IdMineral = idMineral;
+            Cantidad = peso;
+            Total = total;
+            DetalleEntrada = detalleEntrada;
+            fechaActualizacion = fechaActualizacion;
+        }
+
+        // Métodos
+
+        public EntradaMineral CargarOro()
+        {
+            // Crear el objeto que almacena la información de los resultados
+            EntradaMineral mineral = new EntradaMineral();
+
+            try
+            {
+                // Query de selección
+                string query = @"select  [idProducto],sum([cantidad]) as 'Total K Ingresados', sum([Total]) as 'Total'  from [Minas].[Entrada] 
+                                 where  [idProducto] =1 group by [idProducto] ";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("idProductol", 1);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del mineral si la consulta retorna valores
+                        mineral.IdMineral = Convert.ToInt32(rdr["idProducto"]);
+                        mineral.Cantidad = Convert.ToDouble(rdr["Total K Ingresados"]);
+                        mineral.Total = Convert.ToDecimal(rdr["Total"]);
+                    }
+                }
+
+                // Retornar el mineral con los valores
+                return mineral;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// extraemos la informacion del mineral Zinz
+        /// </summary>
+        /// <returns> la informacion del inventario del zinc </returns>
+
+        public EntradaMineral CargarZinc()
+        {
+            EntradaMineral mineral1 = new EntradaMineral();
+
+            try
+            {
+                // Query de selección
+                string query = @"select  [idProducto],sum([cantidad]) as 'Total K Ingresados', sum([Total]) as 'Total'  from [Minas].[Entrada] 
+                                 where  [idProducto] =2 group by [idProducto] ";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("idProducto", 2);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del mineral si la consulta retorna valores
+                        mineral1.IdMineral = Convert.ToInt32(rdr["idProducto"]);
+                        mineral1.Cantidad = Convert.ToDouble(rdr["Total K Ingresados"]);
+                        mineral1.Total = Convert.ToDecimal(rdr["Total"]);
+                    }
+                }
+
+                // Retornar el mineral con los valores
+                return mineral1;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// se cargan los valores del plomo
+        /// </summary>
+        /// <returns>la informacion del mineral de plomo </returns>
+
+        public EntradaMineral CargarPlomo()
+        {
+            EntradaMineral mineral1 = new EntradaMineral();
+
+            try
+            {
+                // Query de selección
+                string query = @"select  [idProducto],sum([cantidad]) as 'Total K Ingresados', sum([Total]) as 'Total'  from [Minas].[Entrada] 
+                                 where  [idProducto] =3 group by [idProducto] ";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("idProductol", 3);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del mineral si la consulta retorna valores
+                        mineral1.IdMineral = Convert.ToInt32(rdr["idProducto"]);
+                        mineral1.Cantidad = Convert.ToDouble(rdr["Total K Ingresados"]);
+                        mineral1.Total = Convert.ToDecimal(rdr["Total"]);
+                    }
+                }
+
+                // Retornar el mineral con los valores
+                return mineral1;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// se cargan los datos del mineral de plata 
+        /// </summary>
+        /// <returns></returns>
+        public EntradaMineral CargarPlata()
+        {
+            EntradaMineral mineral1 = new EntradaMineral();
+
+            try
+            {
+                // Query de selección
+                string query = @"select  [idProducto],sum([cantidad]) as 'Total K Ingresados', sum([Total]) as 'Total'  from [Minas].[Entrada] 
+                                 where  [idProducto] =4 group by [idProducto] ";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("idProducto", 4);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del mineral si la consulta retorna valores
+                        mineral1.IdMineral = Convert.ToInt32(rdr["idProducto"]);
+                        mineral1.Cantidad = Convert.ToDouble(rdr["Total K Ingresados"]);
+                        mineral1.Total = Convert.ToDecimal(rdr["Total"]);
+                    }
+                }
+
+                // Retornar el mineral con los valores
+                return mineral1;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+        /// <summary>
+        /// se cargan los datos del mineral de cobre
+        /// </summary>
+        /// <returns></returns>
+        public EntradaMineral CargarCobre()
+        {
+            EntradaMineral mineral1 = new EntradaMineral();
+
+            try
+            {
+                // Query de selección
+                string query = @"select  [idProducto],sum([cantidad]) as 'Total K Ingresados', sum([Total]) as 'Total'  from [Minas].[Entrada] 
+                                 where  [idProducto] =5 group by [idProducto] ";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("idProductol", 5);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del mineral si la consulta retorna valores
+                        mineral1.IdMineral = Convert.ToInt32(rdr["idProducto"]);
+                        mineral1.Cantidad = Convert.ToDouble(rdr["Total K Ingresados"]);
+                        mineral1.Total = Convert.ToDecimal(rdr["Total"]);
+                    }
+                }
+
+                // Retornar el mineral con los valores
+                return mineral1;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/Historial_Entradas.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
+namespace ProyectoMinaELMochito
+{
+    /// <summary>
+    /// Lógica de interacción para Historial_Entradas.xaml
+    /// </summary>
+    public partial class Historial_Entradas : Window
+    {
+        private EntradaMineral entradaMineral = new EntradaMineral();
+        public Historial_Entradas()
+        {
+            InitializeComponent();
+            EntradaMineral elmineral = entradaMineral.CargarOro();
+            EntradaMineral elmineralzinc = entradaMineral.CargarZinc();
+            EntradaMineral elmineralplata = entradaMineral.CargarPlata();
+            EntradaMineral elmineralcobre = entradaMineral.CargarCobre();
+            EntradaMineral elmineralPlomo = entradaMineral.CargarPlomo();
+            SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+
+                    Title = $"Cobre , K:{elmineralcobre.Cantidad}, valor en ${elmineralcobre.Total.ToString("N")}",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble( elmineralcobre.Cantidad)), new  ObservableValue (Convert.ToDouble(elmineralcobre.Total)) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = $"Plata , K:{elmineralplata.Cantidad}, valor en ${elmineralplata.Total.ToString("N")}",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble(elmineralplata.Cantidad)) , new  ObservableValue (Convert.ToDouble(elmineralplata.Total))},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = $"Oro, K:{elmineral.Cantidad}, valor en ${elmineral.Total.ToString("N")}",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble(elmineral.Cantidad)) , new  ObservableValue (Convert.ToDouble(elmineral.Total)) },
+                    DataLabels = true,
+                    ToolTip= elmineral.Total
+                },
+                new PieSeries
+                {
+                    Title =$"Zinc, K:{elmineralzinc.Cantidad}, valor en ${elmineralzinc.Total.ToString("N")}",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble(elmineralzinc.Cantidad)), new  ObservableValue (Convert.ToDouble(elmineralzinc.Total)) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title =$"Plomo , K:{elmineralPlomo.Cantidad}, valor en ${elmineralPlomo.Total.ToString("N")}",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble(elmineralPlomo.Cantidad)), new  ObservableValue (Convert.ToDouble(elmineralPlomo.Total)) },
+                    DataLabels = true
+                }
+            };
+
+            //adding values or series will update and animate the chart automatically
+            //SeriesCollection.Add(new PieSeries());
+            //SeriesCollection[0].Values.Add(5);
+
+            DataContext = this;
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+
+        private void UpdateAllOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+
+            foreach (var series in SeriesCollection)
+            {
+                foreach (var observable in series.Values.Cast<ObservableValue>())
+                {
+                    observable.Value = r.Next(0, 10);
+                }
+            }
+        }
+
+        private void AddSeriesOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            var c = SeriesCollection.Count > 0 ? SeriesCollection[0].Values.Count : 5;
+
+            var vals = new ChartValues<ObservableValue>();
+
+            for (var i = 0; i < c; i++)
+            {
+                vals.Add(new ObservableValue(r.Next(0, 10)));
+            }
+
+            SeriesCollection.Add(new PieSeries
+            {
+                Values = vals
+            });
+        }
+
+        private void RemoveSeriesOnClick(object sender, RoutedEventArgs e)
+        {
+            if (SeriesCollection.Count > 0)
+                SeriesCollection.RemoveAt(0);
+        }
+
+        private void AddValueOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            foreach (var series in SeriesCollection)
+            {
+                series.Values.Add(new ObservableValue(r.Next(0, 10)));
+            }
+        }
+
+        private void RemoveValueOnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var series in SeriesCollection)
+            {
+                if (series.Values.Count > 0)
+                    series.Values.RemoveAt(0);
+            }
+        }
+
+        private void RestartOnClick(object sender, RoutedEventArgs e)
+        {
+            Chart.Update(true, true);
+        }
+    }
+}
