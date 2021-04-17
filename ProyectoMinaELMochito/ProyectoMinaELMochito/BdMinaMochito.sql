eb86d170dc8371b301048bcec2f9fd7046720251
Ernesto Flores <ernesto.16floreshernandez@gmail.com>
2021-04-16 22:56:30 -0600

    Base de datos final!!!

--- ProyectoMinaELMochito/ProyectoMinaELMochito/BdMinaMochito.sql
+++ ProyectoMinaELMochito/ProyectoMinaELMochito/BdMinaMochito.sql
@@ -1,4 +1,4 @@
-/*Base de datos Mina el MOchito*/
+/*Base de datos Mina el Mochito*/
 
 use tempdb
 go
@@ -36,7 +36,7 @@ go
 create table Minas.cargo
 (
 	idCargo int identity not null,
-	descripcion varchar(15),
+	descripcion varchar(30),
 	Constraint PK_Cargo_id
 	       primary key clustered(idCargo)
 )
@@ -56,7 +56,7 @@ create table Minas.Vehiculo
 (
 	idVehiculo int identity  not null,
 	marca varchar(15),
-	modelo varchar(15),
+	modelo varchar(30),
 	placa varchar(15) not null,
 	color varchar(15),
 	estado int ,
@@ -284,20 +284,39 @@ BEGIN
 END
 GO
 
--- Trigger par calcular el total de una producción despues de insertar el peso
-Create Trigger Minas.TotalProduccion
-On Minas.Produccion
-After Insert
+-- Trigger para incrementar el inventario en caso de una eliminacion en la salida
+Create Trigger Minas.BorrarProductoSalida
+On Minas.Salida
+After delete
 As
 Begin
-	Declare @peso decimal, @precio decimal, @total decimal, @idProduccion int
-    Select @peso = peso from inserted
-	Select @precio = precio from inserted
-	Select @idProduccion = idProduccion from inserted
-	   Set @total = @peso * @precio
-    Update Minas.Produccion set @total = total where idProduccion = @idProduccion
+	Declare @peso decimal, @precio decimal, @total decimal, @idMineral int
+	Select @idMineral = idmineral from deleted
+	Select @total = total from deleted
+	select @peso = cantidad from deleted
+    Update Minas.InventarioMineral set peso = peso + @peso ,Total =Total + @total where idMineral = @idMineral
 END
 Go
+
+
+-- Trigger que restablece el inventario si es que se elimina una producción
+CREATE TRIGGER Minas.restablecerInventario
+ON [Minas].Produccion
+AFTER delete
+AS
+BEGIN
+	DECLARE @cantidad decimal,@total decimal , @idMineral int
+	SELECT @cantidad = peso from deleted
+	select @total = total from deleted
+	update [Minas].[InventarioMineral]
+	set
+	[peso] =  [peso] - @cantidad, 
+	[fechaActualizacion]= SYSUTCDATETIME(),
+	[Total]=[Total]-@Total
+	where [idMineral] = @idMineral
+END
+GO
+
 
 ----------------------------------------------------------------Inserciones------------------------------------------------------
 
@@ -310,29 +329,31 @@ go
 --select * from Minas.Genero
 
 insert into Minas.cargo values
-('Gerente'),
-('escabador')
+('Gerente'),('Secretarios'),('Supervisor de Area'),('Guardia'),('Conductor'),('Minero'),('conductor Maquinaria')
 go
 
-
+insert into minas.EstadoVehiculo values ('Activo'),('Reparacion'),('Inactivo')insert into minas.Vehiculo values ('Caterpillar','Retroexcabadora','AQE1265','Amarillo',1),('Caterpillar','Retroexcabadora','QQXSQ23','Amarillo',2),('XCMG','Retroexcabadora','ASDJU15','Amarillo',1),('Caterpillar','Cargadora','QQXSQ23','Naranja',1),('xcmg','Billdozers','OTIR159','Amarillo',2),('xcmg','Billdozers','NVJGO84','Amarillo',1),('xcmg','Motoniveladoras','PYNGI10','Amarillo',1)
+go
 
 insert into [Minas].[Mineral] ([descripcion],[precio]) values
-('ORO',1174.44),
-('PLATA', 10678.68)
+('Oro',56182.88),('Zinc',2809.50),('Plomo',1984.50),('Plata',836.88),('Cobre',9187.50)
+go
+
+insert into [Minas].[InventarioMineral] ([idMineral],[peso], [fechaActualizacion], [Total]) values
+(1,00,null,00),
+(2,00,null,00),
+(3,00,null,00),
+(4,00,null,00),
+(5,00,null,00)
 go
 
 insert into [Minas].[EstadoVehiculo] ([descripcion]) values
 ('Disponible')
 go 
 
-insert into [Minas].[Vehiculo] ([marca],[modelo],[placa],[color],[estado]) values
-('MACK', 'MACK 4X4','12wer12s','rojo',1),
-('CAT', '795F CA','1567ygays','amarilloo',1)
-go
-
 insert into [Minas].[Empleado] ([identidad],[primerNombre],[edad],[idGenero],direccion,[idCargo],[salario],[estado]) values 
-('0318200300249','lionel messi','33',1,'barrio no te encuentro',1,10000,1),
-('1627865479101','Victor Madrid','30',1,'Morazan',1,10678,1)
+('0318200300249','lionel messi','33',1,'Barrio Guamilito',1,10000,1),
+('1627865479101','Victor Madrid','30',1,'Barrio Morazan',1,10678,1)
 go
 
 insert into [Minas].[viajeInterno]([idVehiculo],[idEmpleado])values 
@@ -340,9 +361,8 @@ insert into [Minas].[viajeInterno]([idVehiculo],[idEmpleado])values
 (2, 2)
 go
 
-delete from Minas.Produccion where idProduccion = 3
 insert into [Minas].[Produccion]([idViaje],[idMineral],[precio],[peso])Values
-(1,1,10678.68,11.5)
+(1,1,56182.88,11.5)
 go
 
 insert into [Minas].[InventarioMineral]([idMineral],[peso],[fechaActualizacion],[Total])values
@@ -350,161 +370,12 @@ insert into [Minas].[InventarioMineral]([idMineral],[peso],[fechaActualizacion],
 go
 
 
----------------------------------------------------------------Procedimientos Almanenados----------------------------------------
-/* Procedimiento para Usuarios (Agregar Nuevo usuario)*/
-
-
-CREATE PROCEDURE agregarUsuarios (
-
-@nombreCompleto varchar(225),
-@username varchar(100),
-@paswrod varchar(100),
-@rol char (25),
-@estado bit 
-)
-
-AS
-BEGIN
-if exists (select username from Usuarios.Usuario where username= @username and estado=1)
-raiserror ('Ya existe un usuario con ese usuario, porfavor ingrese uno nuevo',16,1)
-else
-insert into Usuarios.Usuario( nombreCompleto,username,password,rol,estado) 
-values(@nombreCompleto,@username,@paswrod,@rol,@estado)
-END
-go
-
-/*procedimiento para actualizar un usuario*/
-drop procedure actualizarUsuario
-go
-create procedure actualizarUsuario (
-@id as int ,
-@nombreCompleto varchar(225),
-@username varchar(100),
-@paswrod varchar(100),
-@rol char (25),
-@estado bit 
-)
-
-AS
-BEGIN
-
-update Usuarios.Usuario
-set nombreCompleto  = @nombreCompleto, username = @username,
-password  = @paswrod, rol=@rol,estado=@estado
-where id = @id
-end 
-go
-
-----crear procedimiento eliminar usuario
-
-create procedure eliminarUsuario(@id int )
-AS
-BEGIN
-
-update Usuarios.Usuario set estado =0
-where  id =@id
-end 
-go
-
-/*procedimiento ´para buscar usuario*/
-create procedure buscarUser
-@nombre as varchar(225)
-as begin
-select *
-from Usuarios.Usuario
-where nombreCompleto like '%' +@nombre+ '%'
-end 
+Insert into Minas.Salida ([idmineral],[cantidad],[Total],[detalleVenta],[fechaSalida]) values 
+(1, 1, 56182.88, 'Salio 1 kg de oro', GETDATE())
 go
 
 
+delete from Minas.Produccion where idProduccion = 1
 
-----------------------------------------CRUD Empleado
---Crear
-create procedure CrearEmpleado
-@identidad varchar(13),
-@primerNombre varchar(20),
-@segundoNombre varchar(20),
-@primerApellido varchar(20),
-@segundoApellido varchar(20),
-@edad int,
-@idGenero int,
-@direccion  varchar(50),
-@idPuesto int
-as
-begin
-insert	into Minas.Empleado values(@identidad,@primerNombre,@segundoNombre,@primerApellido,@segundoApellido,@edad,@idGenero,@direccion,@idPuesto,'activo')
-end
-go
 
-create procedure verEmpleados
-as begin
-select E.IdEmpleado AS 'Empleado ID',
-	   E.identidad,
-	   E.primerNombre as 'Primer Nombre',
-	   E.segundoNombre as 'Segundo Nombre',
-	   E.primerApellido as 'Primer Apellido',
-	   E.segundoApellido as 'Segundo Apellido',
-	   E.edad,
-	   G.descripcion as 'Genero',
-	   E.direccion,
-	   C.descripcion as 'Cargo'
-from Minas.Empleado E INNER JOIN  Minas.Genero G
-on G.idGenero = E.idGenero INNER JOIN Minas.cargo C
-on C.idCargo = E.idCargo
-where E.estado = 'activo'
-end
-go
 
---ver empleado Individual
-create procedure verEmpleado(@ID int)
-as 
-begin
-select E.IdEmpleado AS 'Empleado ID',
-	   E.identidad,
-	   E.primerNombre as 'Primer Nombre',
-	   E.segundoNombre as 'Segundo Nombre',
-	   E.primerApellido as 'Primer Apellido',
-	   E.segundoApellido as 'Segundo Apellido',
-	   E.edad,
-	   G.descripcion as 'Genero',
-	   E.direccion,
-	   C.descripcion as 'Cargo'
-from Minas.Empleado E INNER JOIN  Minas.Genero G
-on G.idGenero = E.idGenero INNER JOIN Minas.cargo C
-on C.idCargo = E.idCargo
-where E.IdEmpleado = @ID and  E.estado = 'activo'
-end
-go
-
---Actualizar un empleado
-create procedure ActualizarEmpleado(	
-@primerNombre varchar(20),
-@segundoNombre varchar(20),
-@primerApellido varchar(20) ,
-@segundoApellido varchar(20),
-@edad int,
-@idGenero int ,
-@direccion  varchar(50),
-@idCargo int
-)
-as 
-begin
-update Minas.Empleado set 
-primerNombre = @primerNombre,
-segundoNombre = @segundoNombre,
-primerApellido = @primerApellido,
-segundoApellido = @segundoApellido,
-edad = @edad,
-idGenero =@idGenero,
-direccion =@direccion,
-idCargo = @idCargo
-end
-go
-
---Eliminar un empleado
-create procedure EliminarEmpleado(@ID int)
-as
-begin
-	update Minas.Empleado set estado = 'inactivo'
-end
-go
